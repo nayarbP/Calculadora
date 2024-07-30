bfd846e396b16e9a0cbfc926e5b3f6f7ee5e31e7
frankh091 <59120953+frankh091@users.noreply.github.com>
2024-07-30 12:30:49 -0500

    Update OperacionesBasicas.java
    Se puede comenzar con la implementación.

--- OperacionesBasicas.java
+++ OperacionesBasicas.java
@@ -1,29 +1,35 @@
+package com.mycompany.operacionesbasicas;
 //Librerias para trabajar con una lista de caracteres por patrones de busqueda
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class OperacionesBasicas{
-  //Funcion main provicional
-  public static void main(String [] args){
-    String expresion = "operacion a resolver";
-    double resultado = calcularexpresion(expresion);
-    System.out.println("Resultado:"+resultado);
-     }
+    //Función main provisional
+    public static void main(String [] args){
+        //String expresion = "operacion a resolver";
+        String expresion = "5+34/4-4/0";
+        double resultado = calcularexpresion(expresion);
+        if (!Double.isNaN(resultado)) {
+            System.out.println("Resultado: " + resultado);
+        }
+    }
   
-  public static double calcularexpresion(String expresion){
-     //Aqui se tomara el string de operaciones y se llevaran a las funciones correspondientes
-     String signo="+"; //Signo necesario para multiplicacion y division con valores negativos
+    public static double calcularexpresion(String expresion){
+        // Aquí se tomará el string de operaciones y se llevarán a las funciones correspondientes
+        String signo = "+"; // Signo necesario para multiplicación y división con valores negativos
+
         // Evaluamos las multiplicaciones y divisiones, con valor1 y valor2 positivos
         Pattern pattern = Pattern.compile("(\\d+\\.?\\d*)([*/])(\\d+\\.?\\d*)");
         Matcher matcher = pattern.matcher(expresion);
         while (matcher.find()) {
-            System.out.println("Resultado: " + expresion);//imprime el proceso de la expresion, opcional
+            System.out.println("Expresión: " + expresion); // imprime el proceso de la expresión, opcional
             String operador0 = "";
             double valor1 = Double.parseDouble(matcher.group(1));
             String operador = matcher.group(2);
             String operador1 = "";
             double valor2 = Double.parseDouble(matcher.group(3));
-            double resultado = realizarOperacion(operador0, valor1, operador,operador1, valor2);
+            double resultado = realizarOperacion(operador0, valor1, operador, operador1, valor2);
+            if (Double.isNaN(resultado)) return Double.NaN;
             expresion = expresion.substring(0, matcher.start()) + resultado + expresion.substring(matcher.end());
             matcher = pattern.matcher(expresion);
         }
@@ -31,15 +37,15 @@ public class OperacionesBasicas{
         // Evaluamos las multiplicaciones y divisiones, con valor1 negativo
         pattern = Pattern.compile("([-])(\\d+\\.?\\d*)([*/])(\\d+\\.?\\d*)");
         matcher = pattern.matcher(expresion);
-
         while (matcher.find()) {
-            System.out.println("Resultado: " + expresion);//imprime el proceso de la expresion, opcional
-            String operador0=matcher.group(1);
+            System.out.println("Expresión: " + expresion); // imprime el proceso de la expresión, opcional
+            String operador0 = matcher.group(1);
             double valor1 = Double.parseDouble(matcher.group(2));
             String operador = matcher.group(3);
             String operador1 = "";
             double valor2 = Double.parseDouble(matcher.group(4));
-            double resultado = realizarOperacion(operador0, valor1, operador,operador1, valor2);
+            double resultado = realizarOperacion(operador0, valor1, operador, operador1, valor2);
+            if (Double.isNaN(resultado)) return Double.NaN;
             expresion = expresion.substring(0, matcher.start()) + resultado + expresion.substring(matcher.end());
             matcher = pattern.matcher(expresion);
         }
@@ -47,31 +53,31 @@ public class OperacionesBasicas{
         // Evaluamos las multiplicaciones y divisiones, con valor1 y valor2 negativos
         pattern = Pattern.compile("([-])(\\d+\\.?\\d*)([*/])([-])(\\d+\\.?\\d*)");
         matcher = pattern.matcher(expresion);
-
         while (matcher.find()) {
-            System.out.println("Resultado: " + expresion);//imprime el proceso de la expresion, opcional
-            String operador0=matcher.group(1);
+            System.out.println("Expresión: " + expresion); // imprime el proceso de la expresión, opcional
+            String operador0 = matcher.group(1);
             double valor1 = Double.parseDouble(matcher.group(2));
             String operador = matcher.group(3);
             String operador1 = matcher.group(4);
             double valor2 = Double.parseDouble(matcher.group(5));
-            double resultado = realizarOperacion(operador0, valor1, operador,operador1, valor2);
-            expresion = expresion.substring(0, matcher.start()) + signo +resultado + expresion.substring(matcher.end());
+            double resultado = realizarOperacion(operador0, valor1, operador, operador1, valor2);
+            if (Double.isNaN(resultado)) return Double.NaN;
+            expresion = expresion.substring(0, matcher.start()) + signo + resultado + expresion.substring(matcher.end());
             matcher = pattern.matcher(expresion);
         }
         
         // Evaluamos las multiplicaciones y divisiones, con valor2 negativo
         pattern = Pattern.compile("(\\d+\\.?\\d*)([*/])([-])(\\d+\\.?\\d*)");
         matcher = pattern.matcher(expresion);
-
         while (matcher.find()) {
-            System.out.println("Resultado: " + expresion);//imprime el proceso de la expresion, opcional
-            String operador0="";
+            System.out.println("Expresión: " + expresion); // imprime el proceso de la expresión, opcional
+            String operador0 = "";
             double valor1 = Double.parseDouble(matcher.group(1));
             String operador = matcher.group(2);
             String operador1 = matcher.group(3);
             double valor2 = Double.parseDouble(matcher.group(4));
-            double resultado = realizarOperacion(operador0, valor1, operador,operador1, valor2);
+            double resultado = realizarOperacion(operador0, valor1, operador, operador1, valor2);
+            if (Double.isNaN(resultado)) return Double.NaN;
             expresion = expresion.substring(0, matcher.start()) + resultado + expresion.substring(matcher.end());
             matcher = pattern.matcher(expresion);
         }
@@ -79,15 +85,15 @@ public class OperacionesBasicas{
         // Luego evaluamos las sumas y restas, con valor1 y valor2 negativos
         pattern = Pattern.compile("([-])(\\d+\\.?\\d*)([+-])([-])(\\d+\\.?\\d*)");
         matcher = pattern.matcher(expresion);
-
         while (matcher.find()) {
-            System.out.println("Resultado: " + expresion);//imprime el proceso de la expresion, opcional
-            String operador0="-";
+            System.out.println("Expresión: " + expresion); // imprime el proceso de la expresión, opcional
+            String operador0 = "-";
             double valor1 = Double.parseDouble(matcher.group(2));
             String operador = matcher.group(3);
             String operador1 = "-";
             double valor2 = Double.parseDouble(matcher.group(5));
-            double resultado = realizarOperacion(operador0, valor1, operador,operador1, valor2);
+            double resultado = realizarOperacion(operador0, valor1, operador, operador1, valor2);
+            if (Double.isNaN(resultado)) return Double.NaN;
             expresion = expresion.substring(0, matcher.start()) + resultado + expresion.substring(matcher.end());
             matcher = pattern.matcher(expresion);
         }
@@ -95,15 +101,15 @@ public class OperacionesBasicas{
         // Evaluamos las sumas y restas, con valor2 negativo
         pattern = Pattern.compile("(\\d+\\.?\\d*)([+-])([-])(\\d+\\.?\\d*)");
         matcher = pattern.matcher(expresion);
-
         while (matcher.find()) {
-            System.out.println("Resultado: " + expresion);//imprime el proceso de la expresion, opcional
-            String operador0="";
+            System.out.println("Expresión: " + expresion); // imprime el proceso de la expresión, opcional
+            String operador0 = "";
             double valor1 = Double.parseDouble(matcher.group(1));
             String operador = matcher.group(2);
             String operador1 = "-";
             double valor2 = Double.parseDouble(matcher.group(4));
-            double resultado = realizarOperacion(operador0, valor1, operador,operador1, valor2);
+            double resultado = realizarOperacion(operador0, valor1, operador, operador1, valor2);
+            if (Double.isNaN(resultado)) return Double.NaN;
             expresion = expresion.substring(0, matcher.start()) + resultado + expresion.substring(matcher.end());
             matcher = pattern.matcher(expresion);
         }
@@ -111,15 +117,15 @@ public class OperacionesBasicas{
         // Evaluamos las sumas y restas, con valor1 negativo
         pattern = Pattern.compile("([-])(\\d+\\.?\\d*)([+-])(\\d+\\.?\\d*)");
         matcher = pattern.matcher(expresion);
-
         while (matcher.find()) {
-            System.out.println("Resultado: " + expresion);//imprime el proceso de la expresion, opcional
-            String operador0="-";
+            System.out.println("Expresión: " + expresion); // imprime el proceso de la expresión, opcional
+            String operador0 = "-";
             double valor1 = Double.parseDouble(matcher.group(2));
             String operador = matcher.group(3);
             String operador1 = "";
             double valor2 = Double.parseDouble(matcher.group(4));
-            double resultado = realizarOperacion(operador0, valor1, operador,operador1, valor2);
+            double resultado = realizarOperacion(operador0, valor1, operador, operador1, valor2);
+            if (Double.isNaN(resultado)) return Double.NaN;
             expresion = expresion.substring(0, matcher.start()) + resultado + expresion.substring(matcher.end());
             matcher = pattern.matcher(expresion);
         }
@@ -127,25 +133,25 @@ public class OperacionesBasicas{
         // Evaluamos las sumas y restas, con valor1 y valor2 positivos
         pattern = Pattern.compile("(\\d+\\.?\\d*)([+-])(\\d+\\.?\\d*)");
         matcher = pattern.matcher(expresion);
-
         while (matcher.find()) {
-            System.out.println("Resultado: " + expresion);//imprime el proceso de la expresion, opcional
+            System.out.println("Expresión: " + expresion); // imprime el proceso de la expresión, opcional
             String operador0 = "";
             double valor1 = Double.parseDouble(matcher.group(1));
             String operador = matcher.group(2);
             String operador1 = "";
             double valor2 = Double.parseDouble(matcher.group(3));
-            double resultado = realizarOperacion(operador0, valor1, operador,operador1, valor2);
+            double resultado = realizarOperacion(operador0, valor1, operador, operador1, valor2);
+            if (Double.isNaN(resultado)) return Double.NaN;
             expresion = expresion.substring(0, matcher.start()) + resultado + expresion.substring(matcher.end());
             matcher = pattern.matcher(expresion);
         }
         return Double.parseDouble(expresion);
     }
   
-  //Aqui se realiza la operacion tomando en cuenta los signos
+    // Aquí se realiza la operación tomando en cuenta los signos
     public static double realizarOperacion(String operador0, double valor1, String operador, String operador1, double valor2){
-        //Evaluamos con valor1 negativo
-        if(operador0.equals("-")&&operador1.equals("")){
+        // Evaluamos con valor1 negativo
+        if(operador0.equals("-") && operador1.equals("")){
             switch (operador) {
                 case "+":
                     return -valor1 + valor2;
@@ -154,15 +160,18 @@ public class OperacionesBasicas{
                 case "*":
                     return -valor1 * valor2;
                 case "/":
-                    //Hay que arreglar /0
+                    if (valor2 == 0) {
+                        System.out.println("No se puede dividir por cero.");
+                        return Double.NaN;
+                    }
                     return  -valor1 / valor2;
                 default:
                     throw new IllegalArgumentException("Operador no soportado: " + operador);
-             }
+            }
         }
             
-            //Evaluamos con valor2 negativo
-        if(operador0.equals("")&&operador1.equals("-")){
+        // Evaluamos con valor2 negativo
+        if(operador0.equals("") && operador1.equals("-")){
             switch (operador) {
                 case "+":
                     return valor1 - valor2;
@@ -171,15 +180,18 @@ public class OperacionesBasicas{
                 case "*":
                     return valor1 * -valor2;
                 case "/":
-                    //Hay que arreglar /0
+                    if (valor2 == 0) {
+                        System.out.println("No se puede dividir por cero.");
+                        return Double.NaN;
+                    }
                     return  valor1 / -valor2;
                 default:
                     throw new IllegalArgumentException("Operador no soportado: " + operador);
-             }
+            }
         }
 
-            //Evaluamos con valor1 y valor2 negativos
-        if(operador0.equals("-")&&operador1.equals("-")){
+        // Evaluamos con valor1 y valor2 negativos
+        if(operador0.equals("-") && operador1.equals("-")){
             switch (operador) {
                 case "+":
                     return -valor1 - valor2;
@@ -188,14 +200,17 @@ public class OperacionesBasicas{
                 case "*":
                     return valor1 * valor2;
                 case "/":
-                    //Hay que arreglar /0
+                    if (valor2 == 0) {
+                        System.out.println("No se puede dividir por cero.");
+                        return Double.NaN;
+                    }
                     return  valor1 / valor2;
                 default:
                     throw new IllegalArgumentException("Operador no soportado: " + operador);
-             }
+            }
         }
 
-            //Evaluamos con valor1 y valor2 positivos
+        // Evaluamos con valor1 y valor2 positivos
         else{
             switch (operador) {
                 case "+":
@@ -205,13 +220,14 @@ public class OperacionesBasicas{
                 case "*":
                     return valor1 * valor2;
                 case "/":
-                    //Hay que arreglar /0
+                    if (valor2 == 0) {
+                        System.out.println("No se puede dividir por cero.");
+                        return Double.NaN;
+                    }
                     return  valor1 / valor2;
                 default:
                     throw new IllegalArgumentException("Operador no soportado: " + operador);
             }
         }  
-     }
+    }
 }
-
-//¿Podemos comenzar con la implementación?
